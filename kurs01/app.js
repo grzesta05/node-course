4ff67467ea4e5216d0f8d61a3e3cb793b151b1f0
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2022-10-13 17:22:29 +0200

    kurs01 initialize course project

--- kurs01/app.js
+++ kurs01/app.js
@@ -1,1 +1,13 @@
-const http = require("");
+const http = require("http");
+
+function rqListener(request, response) {
+  console.log(request.url, request.method, request.headers);
+  //process.exit();
+  response.setHeader("Content-Type", "text/html");
+  response.write("<h1>Hello world</h1>");
+  response.end();
+}
+
+const server = http.createServer(rqListener);
+
+server.listen(3000);
